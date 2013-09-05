6b7bff0d7cf3db5b8c27eb9871fd8fe4e58be7e5
Peter Zhang <zozohtnt@gmail.com>
2013-09-05 20:05:06 +0800

    补充合并掉的代码

--- disk.go
+++ disk.go
@@ -2,12 +2,32 @@ package z
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"os"
 	"path"
 	"path/filepath"
 	"strings"
 )
+
+// 根据给定路径获取绝对路径，可以支持 ~ 作为主目录
+func Ph(ph string) string {
+	if IsBlank(ph) {
+		return ""
+	}
+	if strings.HasPrefix(ph, "~") {
+		home := os.Getenv("HOME")
+		if IsBlank(home) {
+			panic(fmt.Sprintf("can not found HOME in envs, '%s' AbsPh Failed!", ph))
+		}
+		ph = fmt.Sprint(home, string(ph[1:]))
+	}
+	s, err := filepath.Abs(ph)
+	if nil != err {
+		panic(err)
+	}
+	return s
+}
 
 // 创建一个目录，如果目录存在或者创建成功，返回 true，否则返回 false
 func Mkdir(ph string) error {
